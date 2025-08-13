5dfb9eaacf0b462af6196edff3eeceae6f359166
Stephan Kulla <git.mail@kulla.me>
2025-08-13 20:06:10 +0200

    chore: Add sourcemap settings

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
 
+// @ts-expect-error This file is run in a Node.js environment
+const mode = process.env.NODE_ENV || 'development'
+
 export default defineConfig({
   html: {
     title: 'Debug Testing',
   },
   output: {
+    sourceMap: {
+      js: mode === 'development' ? 'eval-source-map' : 'source-map',
+      css: true,
+    },
     assetPrefix: '/2025-08-13-debug-testing/',
   },
   plugins: [pluginReact()],
