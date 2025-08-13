e43598a11222c2d888d574d9c0b4010a7bf8e7f1
Stephan Kulla <git.mail@kulla.me>
2025-08-13 20:06:40 +0200

    feat: Add some code to App()

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 
 export default function App() {
+  const a = 2
+  const b = a * a * a
+
   return (
     <main className="prose p-10">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      {b}
     </main>
   )
 }
